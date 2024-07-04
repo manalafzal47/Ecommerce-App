00cb065d904a8087feb440a7250aabfc84faa6df
Manal Afzal <manal.afzal@ontariotechu.net>
2024-07-03 22:34:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import ProductsPage from './ProductsPage';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
